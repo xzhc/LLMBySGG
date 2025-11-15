e29cd108667128650e218a789610643e72211a3f
xzhc <1838998336@qq.com>
2025-11-16 00:03:24 +0800

    python day01

---
+++ python/day01/P03_Var.py
@@ -0,0 +1,16 @@
+"""
+定义：程序执行过程中，其值可以改变的量
+"""
+a = 10
+b= 'zhangsan'
+print(a)
+print(b)
+
+b = 34
+print(b)
+
+# var1 = var2 = var3 = 15
+var1, var2, var3 = 15, 20, 25
+print(var1)
+print(var2)
+print(var3)
